703fa2fc9a4c99e31ed50710ca4c245362361661
Soupon2gou <Soupon2gou@gmail.com>
2024-07-07 23:50:01 +0900

    改良

--- Assets/Script/Game_Manager.cs
+++ Assets/Script/Game_Manager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Game_Manager : MonoBehaviour
@@ -9,14 +10,19 @@ public class Game_Manager : MonoBehaviour
     public GameObject Ball;
     public Text Ga_Txt;
     public GameObject Bck_Par;
+    bool IsGameOver;//ゲームオーバーになっているかを判定する
+    bool IsGameClear;//ゲームクリアしたかを判定する
 
     // Update is called once per frame
+    [System.Obsolete]
     void Update()
     {
         if(Ball !=null && Ball.transform.position.y < -6)
         {
             Destroy(Ball.gameObject);
+            Time.timeScale = 0;
             Ga_Txt.text = "Game Over";
+            IsGameOver = true;
         }
 
         //ゲームクリア    
@@ -24,6 +30,14 @@ public class Game_Manager : MonoBehaviour
         {
             Time.timeScale = 0;
             Ga_Txt.text = "Game Clear";
+            IsGameClear = true;
+        }
+
+        //リスタート
+        if ((IsGameClear || IsGameOver)&& Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Game_Play_Scene");
+            Time.timeScale = 1;
         }
     }
 }
